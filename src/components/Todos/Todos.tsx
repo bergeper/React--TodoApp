eec62d25d6d64afc262dc4b3fdb84b854d809072
Per Berge <per.berge93@gmail.com>
2023-05-22 17:39:09 +0200

    (feat): styling and component for heading added

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import { TodosContext } from '../../contexts/TodosContext';
 import { ViewTodo } from '../ViewTodo/ViewTodo';
-
+import './Todos.scss';
+import { Heading } from '../Heading/Heading';
 export const Todos = () => {
   const todos = useContext(TodosContext);
   return (
     <>
-      <div>
+      <Heading heading='Tasks'></Heading>
+      <div className='todos'>
         {todos.map((todo) => (
           <ViewTodo todo={todo}></ViewTodo>
         ))}
