d583e88260dc547607c5aa3ea88b58033b1e77e2
Per Berge <per.berge93@gmail.com>
2023-05-22 17:39:20 +0200

    (feat): basic styling

--- src/components/ViewTodo/ViewTodo.tsx
+++ src/components/ViewTodo/ViewTodo.tsx
@@ -2,6 +2,7 @@ import { TodosDispatchContext } from '../../contexts/TodosDispatchContext';
 import { Todo } from '../../models/Todo';
 import { useContext } from 'react';
 import { ActionType } from '../../reducers/TodosReducer';
+import './ViewTodo.scss';
 
 interface ITodoProps {
   todo: Todo;
@@ -10,21 +11,27 @@ interface ITodoProps {
 export const ViewTodo = ({ todo }: ITodoProps) => {
   const dispatch = useContext(TodosDispatchContext);
 
-  return (
+  const htmlForTodo = (
     <div
       onClick={() => {
         dispatch({ type: ActionType.TOGGLE, payload: todo.id });
       }}
+      className='todo'
     >
       <input type='checkbox' checked={todo.done} />
-      <span>{todo.task}</span>
+      <span className={todo.done ? 'todo__task--done' : 'todo__task'}>
+        {todo.task}
+      </span>
       <button
         onClick={() => {
           dispatch({ type: ActionType.REMOVE, payload: todo.id });
         }}
+        className='todo__removeBtn'
       >
         Remove
       </button>
     </div>
   );
+
+  return <>{htmlForTodo}</>;
 };
